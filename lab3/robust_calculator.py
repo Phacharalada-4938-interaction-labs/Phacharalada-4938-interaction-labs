8b3ba9b5db47f648af8d61af3757ac7052a97e49
Phacharalada-4938-interaction-labs <137374155+Phacharalada-4938-interaction-labs@users.noreply.github.com>
2023-07-13 23:45:13 +0700

    robust_calculator.py

--- lab3/robust_calculator.py
+++ lab3/robust_calculator.py
@@ -1,31 +1,31 @@
-def robust_calculator(op1, op2, operator='+', output_format='float'):
-    if not isinstance(op1, (float,int)) or not isinstance(op2, (float,int)):
+def robust_calculator(op1, op2, operator, output_format):
+
+    if not isinstance(op1, (float, int)) or not isinstance(op2, (float, int)):
         raise ValueError('Invalid operands. Operands must be numbers.')
 
     if operator not in ['+', '-', '*', '/']:
         raise ValueError("Operator must be +, -, *, or /")
-    
-    
-    if operator=="+":
-        result=op1 + op2
-    elif operator=="-":
-        result=op1-op2
-    elif operator=="*":
-        result=op1*op2
-    elif operator=="/":
-        if op2==0:
-            raise ValueError("Division by zero is not allowed.")
-        result=op1/op2
-    
-    if output_format=="integer":
-        result=round(result)
-    
+
+    if operator == "+":
+        result = op1 + op2
+    elif operator == "-":
+        result = op1 - op2
+    elif operator == "*":
+        result = op1 * op2
+    elif operator == "/":
+        result = op1 / op2
+
+    if output_format == "float":
+        result = float(result)
+    elif output_format == "int":
+        result = round(result)
+
     return result
 
 
 def get_operand(message):
-    operand=input(message)
-    if operand.lower()=="q":
+    operand = input(message)
+    if operand.lower() == "q":
         exit()
     try:
         return float(operand)
@@ -35,7 +35,7 @@ def get_operand(message):
 
 def get_operator():
     operator = input("Enter an operation ('+', '-', '*', or '/'):")
-    if operator.lower()=="q":
+    if operator.lower() == "q":
         exit()
     if operator not in ["+", "-", "*", "/"]:
         raise ValueError("Operator must be +, -, *, or /")
@@ -55,16 +55,20 @@ def calculator():
             op2 = get_operand("Enter the second operand ('q' to quit):")
             operator = get_operator()
             requested_format = get_format()
-            output = robust_calculator(op1, op2, operator, requested_format)
-            if op1==0:
-                print('Cannot divide by zero')
-                print('We cannot perform your requested calculation')
-            if op2==0:
-                print('Cannot divide by zero')
-                print('We cannot perform your requested calculation')
-            if operator=='':
-                operator='+'
-            print("{} {} {} = {:.1f}".format(op1, operator, op2, output))
+            if op2 == 0:
+                EE = '1'
+            if requested_format not in ["int", "float"]:
+                EE = '2'
+
+            if EE == '1':
+                print("Cannot divide by zero")
+            elif EE == '2':
+                print("Cannot divide by zero")
+                print("We cannot perform your requested calculation")
+            else:
+                output = robust_calculator(op1, op2, operator, requested_format)
+                print("{} {} {} = {:.1f}".format(op1, operator, op2, output))
+
         except ValueError as e:
             print("Error:", str(e))
 
